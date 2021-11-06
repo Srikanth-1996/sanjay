21240f7c3e5a03eddb27e35695c2477808bfe25a
Your Name <you@example.com>
2021-11-06 23:56:03 +0530

    loops and conditional statements

---
+++ loops.py
@@ -0,0 +1,14 @@
+# for i in range(10):
+#     print(i)
+
+
+# for i in range(20,30):
+#     if i==25:
+#         print("I am going to break the loop")
+#         print(i)
+#         break
+
+for i in range(30,35):
+    if i==31:
+        continue
+    print(i)
